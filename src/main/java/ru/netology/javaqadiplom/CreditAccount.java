e11ea5450809f98d0b2c0c35f3bfcdc043573933
Irina D <RabotaYd2021@yandex.ru>
2024-04-10 13:46:27 +0300

    Fixed bugs in СreditAccount

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -46,6 +46,7 @@ public class CreditAccount extends Account {
             balance = -amount;
             return true;
         } else {
+            balance = balance + amount;
             return false;
         }
     }
@@ -66,7 +67,7 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = amount;
+        balance = balance + amount;
         return true;
     }
 
@@ -80,7 +81,11 @@ public class CreditAccount extends Account {
      */
     @Override
     public int yearChange() {
-        return balance / 100 * rate;
+        if (balance < 0) {
+            return balance / 100 * rate;
+        } else {
+            return 0;
+        }
     }
 
     public int getCreditLimit() {
